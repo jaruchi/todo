86cb46b1069b78b92a4f419197497def17d2a5f6
Ruchi Agarwal <ruchismailbox@gmail.com>
2021-10-10 17:05:47 -0500

    updated in branch1

--- js/header.js
+++ js/header.js
@@ -1,1 +1,8 @@
-const header = "rahul";
+function setHeader() {
+    //
+    const h1 = document.createElement("h1");
+    h1.innerText = "TO-DOs : One Stop For All Your Errands";
+    const headerElm = document.querySelector(".header");
+    headerElm.appendChild(h1);
+}
+
